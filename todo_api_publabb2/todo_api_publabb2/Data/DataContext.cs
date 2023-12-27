1c6aac0ef444cc100254949bbee34f69b47ef213
Libre255 <55764810+Libre255@users.noreply.github.com>
2023-12-27 23:58:39 +0100

    added connection to comos db with .net API and added keyvault

--- todo_api_publabb2/todo_api_publabb2/Data/DataContext.cs
+++ todo_api_publabb2/todo_api_publabb2/Data/DataContext.cs
@@ -4,17 +4,28 @@ namespace todo_api_publabb2.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
+        public DbSet<Todo> Todos { get; set; }
 
+        private readonly IConfiguration _configuration;
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=tododb;Trusted_Connection=true;TrustServerCertificate=true;");
+            var cosmosEndPoint = _configuration["cosmos-kv-endpoint-bv"];
+            var cosmosPrimaryKey = _configuration["cosmos-kv-pk-bv"];
+            var cosmosDatabaseName = _configuration["cosmos-kv-db-name-bv"];
+            optionsBuilder.UseCosmos(cosmosEndPoint, cosmosPrimaryKey, cosmosDatabaseName);
         }
 
-        public DbSet<Todo> Todos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>()
+                        .ToContainer("todo-cosmos-container-bv")
+                        .HasPartitionKey(c => c.id);
+        }
+
     }
 }
--- todo_api_publabb2/todo_api_publabb2/Program.cs
+++ todo_api_publabb2/todo_api_publabb2/Program.cs
@@ -1,11 +1,14 @@
 global using todo_api_publabb2.Models;
+using Azure.Identity;
 using todo_api_publabb2.Data;
 using todo_api_publabb2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string keyvaultendpoint = builder.Configuration["KEY_VAULT_END_POINT"];
+builder.Configuration.AddAzureKeyVault(new Uri(keyvaultendpoint), new DefaultAzureCredential());
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
