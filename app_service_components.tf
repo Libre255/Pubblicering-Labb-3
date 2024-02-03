9445c901b46acca1667739a1aedfca88f3766f89
Libre255 <55764810+Libre255@users.noreply.github.com>
2024-02-03 23:00:57 +0100

    added new web app serivce

--- app_service_components.tf
+++ app_service_components.tf
@@ -7,8 +7,49 @@ resource "azurerm_service_plan" "sevice-plan" {
   depends_on          = [azurerm_resource_group.rg-pubblicering-labb3]
 }
 
-resource "azurerm_linux_web_app" "webb-app" {
-  name                = "todo-webb-app-bv"
+resource "azurerm_linux_web_app" "todo-frontend" {
+  name                = "todo-frontend-bv"
+  resource_group_name = local.rg-name
+  location            = local.rg-location
+  service_plan_id     = azurerm_service_plan.sevice-plan.id
+
+  site_config {
+    application_stack {
+      node_version = "16.18"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  #Logic App
+  app_settings = {
+    LOGIC_APP_ENDPOINT = azurerm_logic_app_trigger_http_request.logic-app-request-endpoint.callback_url
+  }
+
+  depends_on = [azurerm_service_plan.sevice-plan, azurerm_cosmosdb_account.cosmos-account, azurerm_logic_app_trigger_http_request.logic-app-request-endpoint]
+}
+
+resource "azurerm_linux_web_app_slot" "todo-frontend-slot" {
+  name           = "todo-frontend-slot-bv"
+  app_service_id = azurerm_linux_web_app.webb-app.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_stack {
+      node_version = "16.18"
+    }
+  }
+
+  depends_on = [azurerm_linux_web_app.webb-app]
+}
+
+resource "azurerm_linux_web_app" "todo-backend" {
+  name                = "todo-backend-bv"
   resource_group_name = local.rg-name
   location            = local.rg-location
   service_plan_id     = azurerm_service_plan.sevice-plan.id
@@ -30,27 +71,6 @@ resource "azurerm_linux_web_app" "webb-app" {
     #this dubbel check if correct
     value = azurerm_cosmosdb_account.cosmos-account.primary_sql_connection_string
   }
-  #Logic App
-  app_settings = {
-    LOGIC_APP_ENDPOINT = azurerm_logic_app_trigger_http_request.logic-app-request-endpoint.callback_url
-  }
 
   depends_on = [azurerm_service_plan.sevice-plan, azurerm_cosmosdb_account.cosmos-account, azurerm_logic_app_trigger_http_request.logic-app-request-endpoint]
-}
-
-resource "azurerm_linux_web_app_slot" "webb-app-slot" {
-  name           = "todo-webb-app-slot-bv"
-  app_service_id = azurerm_linux_web_app.webb-app.id
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  site_config {
-    application_stack {
-      dotnet_version = "7.0"
-    }
-  }
-
-  depends_on = [azurerm_linux_web_app.webb-app]
 }
--- variables.tf
+++ variables.tf
@@ -2,4 +2,5 @@ variable "office365-account-name" {
   description = "Insert outlook account id"
   type        = string
   sensitive   = false
+  default = "brian.gabriel.bartha.veliz@iths.se"
 }
